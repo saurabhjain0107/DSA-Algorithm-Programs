426dcd71b5fe38c57cba4350e9a6b379e3b9a667
saurabh jain <luckyjain0107@gmai.com>
2022-07-27 21:47:16 +0530

    Welcome to Algoritham program

---
+++ Mainclass.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.datastructure;
+
+public class Mainclass {
+    public static void main(String[] args) {
+        System.out.println("welcome to algoritham programs");
+    }
+}
